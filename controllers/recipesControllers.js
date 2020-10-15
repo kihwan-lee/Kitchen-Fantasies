f1a4e8bedb939c25f372fc23bb5c385adda1e5e9
kihwan-lee <kihwanlee97@gmail.com>
2020-10-15 12:08:01 -0500

    Recipe description shows

--- controllers/recipesControllers.js
+++ controllers/recipesControllers.js
@@ -20,13 +20,32 @@ router.get('/new', (req, res) => {
 
 // Create New Recipe
 router.post('/new', (req, res) => {
+  let ingridArr = req.body.ingridients.split(',');
+  let instructArr = req.body.instructions.split(',');
 
-
-  db.Recipe.create(req.body, (err, newRecipe) => {
+  db.Recipe.create({
+    name:req.body.name,
+    cuisineType:req.body.cuisineType,
+    totalCookTime:req.body.totalCookTime,
+    ingridients:ingridArr,
+    instructions:instructArr,
+    user:req.body.user
+    }, (err, newRecipe) => {
     if (err) return console.log(err);
 
     console.log(newRecipe);
     res.redirect('/recipes');
+  });
+});
+
+router.get('/:recipeId', (req, res) => {
+  // Query DB for user
+  db.Recipe.findById(req.params.recipeId, (err, foundRecipe) => {
+    const context = {
+      recipe: foundRecipe,
+    };
+    console.log(foundRecipe);
+    res.render('recipes/show', context);
   });
 });
 
