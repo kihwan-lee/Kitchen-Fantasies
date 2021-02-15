851340c058dd7f64c8e24215106b1f5448abc231
kihwan-lee <kihwanlee97@gmail.com>
2021-02-14 18:09:16 -0600

    Fxing URI issue

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb://localhost:27017/project1';
+const connectionString = process.env.MONGODB_URI;
 
 
 mongoose.connect(connectionString, {
